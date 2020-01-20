052d87b1e7a6c5bbe462fc3139caaee910096ac6
Elvis Blanco <elvisblanco1993@gmail.com>
2020-01-20 17:25:46 -0500

    ...

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -2,4 +2,8 @@ const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 const product = urlParams.get('status');
 
-console.log(product);
+if (product === 'hey') {
+  console.log(product);
+} else {
+  console.log('Nahh');
+}
