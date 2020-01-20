d0e6aa5f329dfb6ccab6752f6b6644047fc4cb90
Elvis Blanco <elvisblanco1993@gmail.com>
2020-01-20 17:41:54 -0500

    Form submission now returns a status message after submission.

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -2,8 +2,18 @@ const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 const product = urlParams.get('status');
 
-if (product === 'hey') {
-  console.log(product);
-} else {
-  console.log('Nahh');
+switch (product) {
+  case 'formsent':
+    document.getElementById('msg').innerHTML = "<p class='msg-success'>Your inquiry has been successfully recorded. We will be contacting you shortly.</p>";
+    window.setTimeout(function(){
+      window.location.href = "https://registrac.page/contact";
+    }, 10000);
+    break;
+  case 'error':
+    document.getElementById('msg').innerHTML = "<p class='msg-error'>Ooops. There was an error and we could not process your request. Please try again later.</p>";
+    window.setTimeout(function(){
+      window.location.href = "https://registrac.page/contact";
+    }, 10000);
+    break;
+  default:
 }
